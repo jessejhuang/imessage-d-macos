cec762cbd3218441eabf2830c70ff5ae364ced45
Jesse Huang <jessehuang@wustl.edu>
2018-03-02 19:58:50 -0600

    first commit

---
+++ src/src/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  src
+//
+//  Created by Jesse Huang on 3/2/18.
+//  Copyright © 2018 Jesse Huang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ src/src/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  src
+//
+//  Created by Jesse Huang on 3/2/18.
+//  Copyright © 2018 Jesse Huang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
